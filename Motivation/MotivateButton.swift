5461a028d801f4270d3f0400c117ae96d3a0e6c1
James Menkal <gojamesgo85@gmial.com>
2022-11-15 20:05:09 -0700

    second commit

---
+++ Motivation/MotivateButton.swift
@@ -0,0 +1,31 @@
+//
+//  MotivateButton.swift
+//  Motivation
+//
+//  Created by James on 11/15/22.
+//
+
+import UIKit
+
+class MotivateButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderWidth = 1
+        backgroundColor = .systemRed
+        setTitle("Motivate me!", for: .normal)
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}
